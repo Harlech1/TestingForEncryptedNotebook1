f516a56e56b36b00f4dd0d078c1a990d0f092067
Türker Kızılcık <turkerkizilcik@gmail.com>
2023-08-15 01:32:28 +0300

    Switch case'leri ile güçlendirildi, proje daha anlaşılır kılındı.

--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
         signInButton.isHidden = !hasPassword
     }
     
-    
+    //*** anlaşıldı ***
     @IBAction func signInButtonClicked(_ sender: Any) {
         
         if let enteredPassword = passwordTextField.text {
@@ -48,51 +48,83 @@ class ViewController: UIViewController {
                     kSecMatchLimit as String: kSecMatchLimitOne
                 ]
                 
-                var item: CFTypeRef?
-                let status = SecItemCopyMatching(query as CFDictionary, &item)
-                
-                if status == errSecSuccess, let passwordData = item as? Data, let password = String(data: passwordData, encoding: .utf8) {
+            
+            var item: CFTypeRef?
+            let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &item)
+
+            switch status {
+            case errSecSuccess:
+                if let passwordData = item as? Data, let password = String(data: passwordData, encoding: .utf8) {
                     if enteredPassword == password {
                         performSegue(withIdentifier: "toList", sender: nil)
                     } else {
-                        makeAlert(titleInput: "Password doesn't match.", messageInput: "Try another password.")
+                        makeAlert(titleInput: "Error", messageInput: "Password doesn't match.")
                     }
                 } else {
-                    makeAlert(titleInput: "Error!", messageInput: "Restart your app and try again.")
+                    makeAlert(titleInput: "Error", messageInput: "Failed to retrieve password data.")
                 }
+            case errSecItemNotFound:
+                makeAlert(titleInput: "Error", messageInput: "Item not found in Keychain.")
+            case errSecAuthFailed:
+                makeAlert(titleInput: "Error", messageInput: "Authentication failed.")
+            case errSecUserCanceled:
+                makeAlert(titleInput: "Error", messageInput: "User canceled the operation.")
+            default:
+                makeAlert(titleInput: "Error", messageInput: "An unknown error occurred.")
             }
+
+        }
         
     }
     
-    
+    //*** anlaşıldı ***
     @IBAction func signUpButtonClicked(_ sender: Any) {
         
         if (passwordTextField.text?.count ?? 0) >= 4 {
             if let password = passwordTextField.text {
-                        let passwordData = password.data(using: .utf8)!
+                
+                let passwordData = password.data(using: .utf8)!
+                let query: [String: Any] = [
+                        kSecClass as String: kSecClassGenericPassword,
+                        kSecAttrAccount as String: "userPassword",
+                        kSecValueData as String: passwordData
+                ]
                         
-                        let query: [String: Any] = [
-                            kSecClass as String: kSecClassGenericPassword,
-                            kSecAttrAccount as String: "userPassword",
-                            kSecValueData as String: passwordData
-                        ]
-                        
-                        let status = SecItemAdd(query as CFDictionary, nil)
-                        
-                        if status == errSecSuccess {
-                            makeAlert(titleInput: "User is created", messageInput: "You can sign in with your password")
-                            signUpButton.isHidden = true
-                            signInButton.isHidden = false
-                        } else {
-                            makeAlert(titleInput: "User couldn't be created!", messageInput: "Restart your app and try again.")
-                        }
+                let status = SecItemAdd(query as CFDictionary, nil)
+            
+                switch status {
+                case errSecSuccess:
+                    makeAlert(titleInput: "Success", messageInput: "Operation completed successfully.")
+                    signUpButton.isHidden = true
+                    signInButton.isHidden = false
+                case errSecDuplicateItem:
+                    makeAlert(titleInput: "Error", messageInput: "This item already exists in the Keychain.")
+                case errSecItemNotFound:
+                    makeAlert(titleInput: "Error", messageInput: "The specified item could not be found in the Keychain.")
+                case errSecAuthFailed:
+                    makeAlert(titleInput: "Error", messageInput: "Authentication failed. Incorrect password or missing authorization.")
+                case errSecDecode:
+                    makeAlert(titleInput: "Error", messageInput: "Data decoding error.")
+                case errSecParam:
+                    makeAlert(titleInput: "Error", messageInput: "Invalid parameter error.")
+                case errSecNotAvailable:
+                    makeAlert(titleInput: "Error", messageInput: "Keychain services are not available.")
+                case errSecUserCanceled:
+                    makeAlert(titleInput: "Error", messageInput: "The user canceled the operation.")
+                case errSecInteractionNotAllowed:
+                    makeAlert(titleInput: "Error", messageInput: "User interaction is not allowed.")
+                case errSecUnimplemented:
+                    makeAlert(titleInput: "Error", messageInput: "Operation is not yet implemented or supported.")
+                default:
+                    makeAlert(titleInput: "Error", messageInput: "Unknown error occurred.")
                 }
+            }
         } else {
             makeAlert(titleInput: "Password can't be shorter than 4 characters!", messageInput: "Try another password.")
         }
         
     }
-    
+    //*** anlaşıldı ***
     func makeAlert(titleInput: String, messageInput : String) {
         let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
@@ -100,6 +132,8 @@ class ViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    
+    //*** anlaşıldı ***
     func checkIfPasswordExists() -> Bool {
             let query: [String: Any] = [
                 kSecClass as String: kSecClassGenericPassword,
